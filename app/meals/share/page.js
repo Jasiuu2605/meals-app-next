d4bb398824c506ed18e0c0a723eb252da5de73c5
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-09-30 19:16:49 +0200

    Add initial project setup with Next.js and essential assets
    - Created package.json for Next.js application with scripts and dependencies
    - Added images: burger.jpg, curry.jpg, dumplings.jpg, logo.png, macncheese.jpg, pizza.jpg, schnitzel.jpg, tomato-salad.jpg to public/images
    - Included additional logo.png in the public directory

---
+++ app/meals/share/page.js
@@ -0,0 +1,5 @@
+function MealsShare() {
+  return <h1>Share Meal Page</h1>;
+}
+
+export default MealsShare;
---
+++ app/page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+import Header from '../components/header';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>sdfsadfasdfdsa</h1>
+      <p>
+        <Link href='/meals'>Meals</Link>
+      </p>
+      <p>
+        <Link href='/meals/share'>Share Meal</Link>
+      </p>
+      <p>
+        <Link href='/community'>Community</Link>
+      </p>
+    </main>
+  );
+}
