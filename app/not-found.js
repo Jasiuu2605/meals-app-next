64a4c1468cd7f1071013573c80d19e80f9bddf8b
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-10-05 14:44:27 +0200

    Add error and not found components for improved user feedback

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Not found</h1>
+      <p>unfortunetly, we could not find requested page or resurce</p>
+    </main>
+  );
+}
+
+export default NotFound;
