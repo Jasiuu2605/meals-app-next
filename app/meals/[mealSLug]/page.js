3b391df828960e4e0ca772e4dc3cbe3800797fdf
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-10-06 19:46:31 +0200

    Add NotFound component and handle meal not found scenario in MealPage

--- app/meals/[mealSLug]/page.js
+++ app/meals/[mealSLug]/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
 import { getMeal } from '@/lib/meals';
 
@@ -6,6 +7,11 @@ import classes from './page.module.css';
 
 function MealPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
   meal.instructions = meal.instructions.replace(/\n/g, '<br>');
   return (
     <>
---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Meal not found</h1>
+      <p>unfortunetly, we could not find requested page or meal data</p>
+    </main>
+  );
+}
+
+export default NotFound;
